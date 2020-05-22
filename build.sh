f9b21b63fa0e308aa4364edd897831f578c5d100
mbearup <mbearup@microsoft.com>
2020-05-22 14:39:31 -0700

    Separate build and restore steps

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash -e
+operation="${1}"
 
+function bail() {
+    if [[ $# -eq 0 ]]; then
+        echo "${0}: Script called 'bail' without passing a message" 1>&2
+    else
+        echo "${0}: $@" 1>&2
+    fi
+    exit 1
+}
 proj=src/DnsClient/DnsClient.csproj
-dotnet restore $proj
-dotnet build $proj
+if [[ -z "${operation}" ]]; then
+    bail "Must supply parameter (restore|build)"
+elif [[ "${operation}" == "restore" ]] || [[ "${operation}" == "build" ]]; then
+    dotnet ${operation} ${proj}
+else
+    bail "Parameter ${operation} not supported"
+fi
 
